8288357d8461465bdc99b4e6fddc0484fe0bbf02
Ananya T <trip.ananya@gmail.com>
2022-11-06 12:43:36 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,38 @@
 
 using namespace vex;
 
+void usercontrol(void) {
+  //user control code 
+  while (1) {
+    
+    //driving forward/backward
+    if(Controller1.Axis2.position(percent) > 20 || Controller1.Axis2.position(percent) < -20) {
+      RightMotorC.spin(fwd, Controller1.Axis2.value(), velocityUnits::pct);
+      LeftMotorA.spin(fwd, Controller1.Axis2.value(), velocityUnits::pct);
+      RightMotorA.spin(fwd, Controller1.Axis2.value(), velocityUnits::pct);
+      RightMotorB.spin(fwd, Controller1.Axis2.value(), velocityUnits::pct);
+    }
+
+    //turning  
+    else if(Controller1.Axis1.position(percent) > 20){
+      RightMotorC.spin(fwd, Controller1.Axis1.value(), velocityUnits::pct);
+      LeftMotorA.spin(fwd, Controller1.Axis1.value(), velocityUnits::pct);
+      RightMotorA.spin(directionType::rev, Controller1.Axis1.value(), velocityUnits::pct);
+      RightMotorB.spin(directionType::rev, Controller1.Axis1.value(), velocityUnits::pct);
+    }
+
+    else {
+      RightMotorC.stop(brake);
+      LeftMotorA.stop(brake);
+      RightMotorA.stop(brake);
+      RightMotorB.stop(brake);
+    }
+
+    wait(20, msec); 
+
+  }
+}
+
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
