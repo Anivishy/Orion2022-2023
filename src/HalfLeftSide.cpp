0f433bbb4ee570e2b43a95f86a592b751ed2b947
Saathvik Somujayabalan <jssaathvik@hotmail.com>
2022-11-10 21:30:02 -0800

    half left side initial route

--- src/HalfLeftSide.cpp
+++ src/HalfLeftSide.cpp
@@ -5,21 +5,21 @@
 #include "driverfunctions.h"
 
 void halfLeftSide() {
+  
+
   // runFlywheel(75, true);
+  // IntakeMotor.spin(reverse, 100, pct);
+  // wait(500, msec);
+  // gyroMove(0.1, -50, 0.0);
+  // wait(1000, msec);
+  // IntakeMotor.stop();
+  // gyroMove(0.15, 50, 0);
+  // turnPID(350, true);
+  // wait(500, msec);
+  // singleShot();
+  // wait(750, msec);
+  // singleShot();
 
-  // //gyroMove(2, 50, 0);
-  // drivePID(100, 0, true);
-  // IntakeMotor.spinFor(-0.25, rev, true);
+  turnPID(-10, true);
 
-  // //gyroMove(-10, 50, 0);
-  // drivePID(500, 0, true);
-  // IndexerSingleShot_Do();
-  // IndexerSingleShot_Do();
-
-  gyroMove(0.1, -50, 0.0);
-  IntakeMotor.spinFor(-1, rev, true);
-  runFlywheel(75, true);
-  gyroMove(0.2, 50, 0);
-
-  //IntakeMotor.spinFor(-0.5, rev, true);
 }
